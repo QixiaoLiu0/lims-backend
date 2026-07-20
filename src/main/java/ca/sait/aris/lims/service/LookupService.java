e46cbc641df2071a360a57f966c1bcd9d9491759
Sasha <oleksandr.maksymenko@edu.sait.ca>
2026-07-20 11:34:55 -0600

    Implemented getSampleTypes inside LookupService

--- src/main/java/ca/sait/aris/lims/service/LookupService.java
+++ src/main/java/ca/sait/aris/lims/service/LookupService.java
@@ -1,11 +1,15 @@
 package ca.sait.aris.lims.service;
 
+import ca.sait.aris.lims.dao.SampleTypeDao;
 import ca.sait.aris.lims.dto.resp.LookupSampleTypeRespDTO;
 import ca.sait.aris.lims.dto.resp.LookupTestTypeRespDTO;
+import ca.sait.aris.lims.util.DBUtil;
 
 import java.util.List;
 
 public class LookupService {
+
+    private final SampleTypeDao sampleTypeDao = new SampleTypeDao();
 
     public List<LookupTestTypeRespDTO> getActiveTestTypes() throws Exception {
     	//TODO
@@ -13,9 +17,14 @@ public class LookupService {
         
     }
 
+    /**
+     * Retrieve all sample types (read-only)
+     */
     public List<LookupSampleTypeRespDTO> getSampleTypes() throws Exception {
-    	//TODO
-		return null;
-        
+    	try {
+            return sampleTypeDao.selectAllSampleTypes();
+        } finally {
+            DBUtil.closeConnection();
+        }
     }
 }
