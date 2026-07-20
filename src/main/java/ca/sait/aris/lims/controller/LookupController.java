65ab028a679c9b52b69a87f84bf177ee2ed34e3a
Sasha <oleksandr.maksymenko@edu.sait.ca>
2026-07-20 11:39:58 -0600

    Implemented API 2: Get Sample Types call inside LookupController

--- src/main/java/ca/sait/aris/lims/controller/LookupController.java
+++ src/main/java/ca/sait/aris/lims/controller/LookupController.java
@@ -3,11 +3,14 @@ package ca.sait.aris.lims.controller;
 import ca.sait.aris.lims.common.RespResult;
 import ca.sait.aris.lims.dto.resp.LookupSampleTypeRespDTO;
 import ca.sait.aris.lims.dto.resp.LookupTestTypeRespDTO;
+import ca.sait.aris.lims.service.LookupService;
 
 import java.util.List;
 
 //sprint 3
 public class LookupController {
+
+    private final LookupService lookupService = new LookupService();
 
     // API 1: Get Active Test Types
     public RespResult<List<LookupTestTypeRespDTO>> getActiveTestTypes() {
@@ -17,11 +20,18 @@ public class LookupController {
         
     }
 
+
     // API 2: Get Sample Types
     public RespResult<List<LookupSampleTypeRespDTO>> getSampleTypes() {
-    	//TODO
-		return null;
-        
-    	
+    	try {
+            List<LookupSampleTypeRespDTO> list = lookupService.getSampleTypes();
+
+            return RespResult.success(list);
+        } catch (Exception e) {
+            System.err.println("[LookupController] getSampleTypes failed " + e.getMessage());
+            e.printStackTrace();
+
+            return RespResult.error("Failed to retrieve sample types");
+        }
     }
 }
