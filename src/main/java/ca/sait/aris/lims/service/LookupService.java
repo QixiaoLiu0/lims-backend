9ba1307ac3021c8c13d5505bffdfc1eaa303996a
Sasha <oleksandr.maksymenko@edu.sait.ca>
2026-07-20 11:08:12 -0600

    implemented getActiveTestTypes in LookupService func

--- src/main/java/ca/sait/aris/lims/service/LookupService.java
+++ src/main/java/ca/sait/aris/lims/service/LookupService.java
@@ -2,15 +2,23 @@ package ca.sait.aris.lims.service;
 
 import ca.sait.aris.lims.dto.resp.LookupSampleTypeRespDTO;
 import ca.sait.aris.lims.dto.resp.LookupTestTypeRespDTO;
+import ca.sait.aris.lims.dao.LookupDao;
+import ca.sait.aris.lims.util.DBUtil;
 
 import java.util.List;
 
 public class LookupService {
 
+    private final LookupDao lookupDao = new LookupDao();
+    /**
+     * Retrieve active test types (read-only)
+     */
     public List<LookupTestTypeRespDTO> getActiveTestTypes() throws Exception {
-    	//TODO
-		return null;
-        
+        try{
+            return lookupDao.selectActiveTestTypes();
+        } finally {
+            DBUtil.closeConnection();
+        }
     }
 
     public List<LookupSampleTypeRespDTO> getSampleTypes() throws Exception {
