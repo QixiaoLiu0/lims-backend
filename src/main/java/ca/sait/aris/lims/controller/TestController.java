f3e81bab9f250ba25ccb5d315721cd316d184de7
Mat <ba55dragonofficial999@gmail.com>
2026-07-23 16:48:43 -0600

    Implemented LIMS-310:
    - deleteTest(String testId)
    - deleteTestById(String testId)
    
    Signed-off-by: Mat <ba55dragonofficial999@gmail.com>

--- src/main/java/ca/sait/aris/lims/controller/TestController.java
+++ src/main/java/ca/sait/aris/lims/controller/TestController.java
@@ -3,11 +3,13 @@ package ca.sait.aris.lims.controller;
 import ca.sait.aris.lims.common.RespResult;
 import ca.sait.aris.lims.dto.resp.TestAssignedRespDTO;
 import ca.sait.aris.lims.dto.resp.TestResultRespDTO;
+import ca.sait.aris.lims.service.TestService;
 
 import java.util.List;
 
 //sprint 3
 public class TestController {
+    private final TestService testService = new TestService();
 
     // API 6: Append Test to Sample
     public RespResult<TestAssignedRespDTO> appendTestToSample(String sampleId, String jsonBody) {
@@ -18,8 +20,14 @@ public class TestController {
 
     // API 8: Delete Test Task
     public RespResult<Object> deleteTest(String testId) {
-    	//TODO
-		return null;
+    	try {
+            testService.deleteTest(testId);
+            return RespResult.success();
+        } catch (Exception e) {
+            System.err.println("[TestController] deleteTest failed: " + e.getMessage());
+            e.printStackTrace();
+            return RespResult.error("Failed to delete test: " + testId);
+        }
         
     }
 
