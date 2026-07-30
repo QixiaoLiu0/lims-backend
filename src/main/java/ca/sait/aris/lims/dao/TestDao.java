64995a222c1136b1aa072a25c54dcd16fa2051e7
Sasha <oleksandr.maksymenko@edu.sait.ca>
2026-07-30 13:39:30 -0600

    implemented getMaxRunNumber func and insertTest func insode TestDao

--- src/main/java/ca/sait/aris/lims/dao/TestDao.java
+++ src/main/java/ca/sait/aris/lims/dao/TestDao.java
@@ -3,12 +3,26 @@ package ca.sait.aris.lims.dao;
 import ca.sait.aris.lims.dto.resp.SampleDetailTestRespDTO;
 import ca.sait.aris.lims.entity.Test;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import ca.sait.aris.lims.util.DBUtil;
 
 public class TestDao extends BaseJdbcDao {
 
     // Do not insert the status field
     public void insertTest(Test test) throws Exception {
-        //TODO
+        String sql = "INSERT INTO test (test_id, sample_id, test_type_id, created_at, run_number, retest_reason) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        executeUpdate(sql,
+                test.getTestId(),
+                test.getSampleId(),
+                test.getTestTypeId(),
+                toTimestamp(test.getCreatedAt()),
+                test.getRunNumber(),
+                test.getRetestReason()
+        );
     }
 
     public void deleteTestById(String testId) throws Exception {
@@ -28,10 +42,28 @@ public class TestDao extends BaseJdbcDao {
         return executeQuery(sql, SampleDetailTestRespDTO.class, sampleId);
     }
 
-    // Dynamic calculation of Run Number
+
+    /**
+     * Dynamic calculation of Run Number
+     * @return -1 if no prior test exists for this sample+testType, so callers do (result + 1)
+     * to get 0 for a first run, 1 for a first retest, etc.
+     */
     public int getMaxRunNumber(String sampleId, Integer testTypeId) throws Exception {
-    	//TODO
-		return 0;
+        String sql = "SELECT COALESCE(MAX(run_number), -1) FROM test WHERE sample_id = ? AND test_type_id = ?";
+
+        Connection conn = DBUtil.getConnection();
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, sampleId);
+            stmt.setInt(2, testTypeId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+            }
+        }
+
+        return -1;
         
     }
 
